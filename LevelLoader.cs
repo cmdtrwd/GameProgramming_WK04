2fabd2e822afeff112d74febf1b60813040a11f3
cmdtrwd <99168914+cmdtrwd@users.noreply.github.com>
2026-09-23 10:05:58 +0700

    Add files via upload

---
+++ LevelLoader.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>(); // Get the Animator component attached to this GameObject
+    }
+
+    void Update()
+    {        
+        if(Input.GetKeyDown(KeyCode.Space)){            
+            anim.SetTrigger("FadeOut"); // Trigger the fade-out animation
+            Invoke("LoadNextScene", 2f);
+        }
+    }
+    // This method can be called from a UI button to start the game
+    public void StartGame(){
+        anim.SetTrigger("FadeOut"); // Trigger the fade-out animation
+        Invoke("LoadNextScene", 2f);
+    }
+    public void LoadNextScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
